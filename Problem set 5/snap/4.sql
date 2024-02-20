e5e94f21afc2d80b8780658fefc4cc96219f0dcf
Mohammad Hafez Najafi <151014739+Hafez-CS@users.noreply.github.com>
2024-02-20 11:38:43 +0330

    Add files via upload

---
+++ Problem set 5/snap/4.sql
@@ -0,0 +1,4 @@
+SELECT "users"."username" FROM "users"
+JOIN "messages" ON "users"."id" = "messages"."from_user_id"
+GROUP BY ("to_user_id")
+ORDER BY COUNT("messages"."from_user_id") DESC,"users"."username" limit 1;
